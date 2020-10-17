5b55452786870539356017a7201cfff47a757860
Jefferson <jeffercs96@gmail.com>
2020-10-17 11:29:23 -0500

    Aplicacion camara con VueNative

---
+++ App.vue
@@ -0,0 +1,46 @@
+<template>
+  <app-navigator></app-navigator>
+</template>
+
+<script>
+import {
+  createAppContainer,
+  createMaterialTopTabNavigator,
+  createBottomTabNavigator,
+  createStackNavigator
+} from "vue-native-router";
+
+import CameraScreen from "./screens/Camera.vue";
+import HomeScreen from "./screens/Home.vue";
+
+const Tabs = createBottomTabNavigator({
+  Camera: HomeScreen,
+});
+
+const StackNavigator = createStackNavigator(
+  {
+    Tabs,
+    Camara: CameraScreen
+  },
+  {
+    defaultNavigationOptions: {
+      gestureEnabled: false,
+      title: "Fotos & Video",
+      headerTitleStyle: {
+        textAlign: "center",
+        alignSelf: "center",
+        color: "white"
+      },
+      headerStyle: {
+        backgroundColor: "black"
+      }
+    }
+  }
+);
+
+const AppNavigator = createAppContainer(StackNavigator);
+
+export default {
+  components: { AppNavigator }
+};
+</script>
